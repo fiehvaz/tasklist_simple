7b7da755b02277ab4ed503ff55b44ec4ba19b140
fiehvaz <vaz.louren@gmail.com>
2021-10-13 00:23:31 -0300

    first release

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'todo_list_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.green),
+      home: TodoListPage(),
+    );
+  }
+}
